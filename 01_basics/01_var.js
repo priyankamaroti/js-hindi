2354609a71075bac3fe15f4c579900d46039aace
priyankamaroti <priyankamaroti04@gmail.com>
2026-09-15 19:33:49 +0000

    datatype

---
+++ 01_basics/01_var.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "priyankamaroti04@gmai.com"
+var accountPassword = "12345"
+accountCity = "hyderabad"
+let accountState;
+
+//accountId = 2//
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "21212"
+
+accountCity = "hyd"
+
+console.log(accountId);
+
+console.table([ accountId , accountEmail ,accountPassword , accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log(3+3);
+console.log("pri");
+
+let name = "pri"
+let age = 21
+let isLoggedin = false
+
+
+console.log(typeof null);
+console.log(typeof undefined);
